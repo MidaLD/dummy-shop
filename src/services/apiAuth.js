2302a6d88bba9c68e5e5b7235a5b454978eef5fc
Lazar Doder <mida992@gmail.com>
2026-02-21 15:45:08 +0100

    xxxxx

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,64 +1,38 @@
-// api.js
-// Funkcije za login, logout i getCurrentUser koristeći dummyjson API
-
-let authToken = null; // čuvamo token nakon login-a
-
-// Login funkcija
 export async function login({ username, password }) {
   const res = await fetch("https://dummyjson.com/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, password }),
+
+    body: JSON.stringify({
+      username,
+      password,
+    }),
   });
 
-  if (!res.ok) {
-    throw new Error("Invalid username or password");
-  }
+  if (!res.ok) throw new Error("Invalid email or password");
 
   const data = await res.json();
-
-  // Sačuvaj token za kasnije
-  authToken = data.token;
 
   return data;
 }
 
-// Logout funkcija
 export async function logout() {
-  if (!authToken) return null;
-
   const res = await fetch("https://dummyjson.com/auth/logout", {
     method: "POST",
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-      "Content-Type": "application/json",
-    },
   });
 
-  if (!res.ok) {
-    throw new Error("Something went wrong on logout");
-  }
+  if (!res.ok) throw new Error("Something went wrong");
 
   const data = await res.json();
-
-  // Očisti token
-  authToken = null;
 
   return data;
 }
 
-// Dohvati trenutno ulogovanog korisnika
 export async function getCurrentUser() {
-  if (!authToken) return null;
-
   const res = await fetch("https://dummyjson.com/auth/me", {
     method: "GET",
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-      "Content-Type": "application/json",
-    },
   });
 
   if (!res.ok) return null;
