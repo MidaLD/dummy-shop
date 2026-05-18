89ba357d20fe4291e063a411009967489b0d55ed
LazarDDR <lazar.doder@outlook.com>
2026-05-18 21:47:32 +0200

    categoriesmenuskeleton fixed

--- src/features/shop/CategoriesMenu.tsx
+++ src/features/shop/CategoriesMenu.tsx
@@ -1,5 +1,4 @@
 import CategoryItem from "./CategoryItem";
-import CategoriesMenuSkeleton from "./CategoriesMenuSkeleton";
 import { useCategoriesList } from "./useCategoriesList";
 import { setSearchQuery, toggleCategoriesMenu } from "../../redux/shopSlice";
 import { useNavigate, useSearchParams } from "react-router";
@@ -89,6 +88,43 @@ function CategoriesMenu({ showCategoriesFinal }: CategoriesMenuProps) {
           className="absolute inset-0 z-10 bg-black/20"
         />
       )}
+    </>
+  );
+}
+
+const ITEM_WIDTHS = [
+  "w-3/4",
+  "w-full",
+  "w-2/3",
+  "w-5/6",
+  "w-1/2",
+  "w-4/5",
+  "w-3/4",
+  "w-2/3",
+  "w-5/6",
+  "w-1/2",
+  "w-4/5",
+  "w-3/4",
+  "w-2/3",
+  "w-5/6",
+  "w-1/2",
+  "w-4/5",
+];
+
+function CategoriesMenuSkeleton() {
+  return (
+    <>
+      <div className="px-4 py-4 border-b border-slate-100">
+        <div className="h-4 w-20 animate-pulse rounded bg-slate-200" />
+      </div>
+
+      <ul className="flex flex-col gap-0.5 p-2">
+        {ITEM_WIDTHS.map((w, i) => (
+          <li key={i} className="px-3 py-2">
+            <div className={`h-5 ${w} animate-pulse rounded bg-slate-200`} />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
