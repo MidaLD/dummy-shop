017f1c758904f1d201f436582fba065117984a96
LazarDDR <lazar.doder@outlook.com>
2026-05-22 16:02:53 +0200

    small bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { lazy, Suspense } from "react";
 import AppLayout from "./ui/AppLayout";
 import CartPage from "./pages/CartPage";
